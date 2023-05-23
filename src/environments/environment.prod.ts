6a22ca43c3abd654782de938331f42244ccb93d2
bocchianaba <45669998+bocchianaba@users.noreply.github.com>
2023-05-23 06:38:51 +0100

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  backend_url: 'http://139.59.208.55:4000'
+  backend_url: 'http://139.59.208.55:4000',
+  backend_socket_url: 'http://139.59.208.55:4000'
 };
